6a2645d35a13379179727ad7a02c09f8b882cc45
sueyoungshim <sueyoungshim@berkeley.edu>
2023-07-18 17:14:10 -0700

    input slider thumb size change worked, clearing the canvas worked

--- public/js/my-drawing-app.js
+++ public/js/my-drawing-app.js
@@ -3,6 +3,7 @@ Feel free to delete any/all of it and replace with your own functionality. */
 
 var path;
 var currentColor = $('#palette').val();
+var strokes = [];
 
 tool.onMouseDown = function(event) { //This code in this function is called whenever the mouse is clicked.
     path = new Path();     // Create a new path each time.
@@ -11,6 +12,8 @@ tool.onMouseDown = function(event) { //This code in this function is called when
     path.strokeWidth = parseInt($('#thickness').val());
     path.strokeCap = 'round';
     path.strokeJoin = 'round';
+    strokes.push(path);
+    console.log(strokes);
     console.log(event.point); //this commands log to the Console the coordinates of the mouse click. Feel free to delete it! 
 }
 tool.onMouseDrag = function(event) {
@@ -47,17 +50,64 @@ $('#thickness').on('input', function() {
     thumb.css('height', value + 'px');
   });
 
-// var thickness = document.getElementById('thickness');
+var thickness = document.getElementById('thickness');
 
-// thickness.oninput = function() {
-//     var thumb = $(this).find('::-webkit-slider-thumb');
-//     var value = parseInt($(this).val());
-//     var thumbSize = value + 'px';
 
-//     if (value > 50) {
-//         thumbSize = 50 + (value - 50) * 2;
-//     }
 
-//     thumb.css('width', thumbSize);
-//     thumb.css('height', thumbSize);
-//   };
+thickness.oninput = function() {
+    var thumb = $(this).find('::-webkit-slider-thumb');
+    var value = $(this).val();
+    var thumbSize = value + 'px';
+
+    for(var j = 0; j < document.styleSheets[2].rules.length; j++) {
+        var rule = document.styleSheets[2].rules[j];
+        if(rule.cssText.match("webkit-slider-thumb")) {
+            rule.style.height=thumbSize;
+            rule.style.width=thumbSize;
+        }
+    }
+  };
+
+
+
+// saving canvas works
+var canvas = document.getElementById('my-canvas');
+var createNewButton = document.getElementById('create-new');
+var imagePreview = document.getElementById('past-works');
+
+createNewButton.addEventListener('click', save);
+
+function clearCanvas() {
+    var context = canvas.getContext('2d');
+    rect = [];
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    console.log(strokes);
+    for (var i = 0; i < strokes.length; i++) {
+        strokes[i].remove();
+    }
+    console.log("cleared");
+}
+
+function save() {
+    console.log("button clicked");
+    // Save the canvas as an image
+    var dataUrl = canvas.toDataURL('image/png');
+  
+    // Create a container div for the image
+    var container = document.createElement('div');
+    container.style = 'padding: 20px; display: inline-block;';
+  
+    // Create an <img> element to display the image
+    var img = document.createElement('img');
+    img.src = dataUrl;
+    img.style = "background-color: white"
+  
+    // Append the image to the container
+    container.appendChild(img);
+  
+    // Append the container to the image preview container
+    imagePreview.appendChild(container);
+
+    console.log("Clearing canvas");
+    clearCanvas();
+  }
